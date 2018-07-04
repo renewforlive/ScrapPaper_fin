73c1c2160e4abb4b04ccc870313aa0fe504c4a6a
renewforlive <renewforlive1@gmail.com>
2018-07-04 18:13:52 +0800

    Initial commit

---
+++ app/src/main/java/com/mycaculate/scrappaper_fin/MainActivity.java
@@ -0,0 +1,13 @@
+package com.mycaculate.scrappaper_fin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
